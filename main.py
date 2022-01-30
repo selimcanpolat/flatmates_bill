975173e42dc1a6dfe9e26e8144b370b579303d4c
Selim Can Polat <selimcanpolat1@gmail.com>
2022-01-30 13:46:46 +0300

    Added generate() method to PdfReport class.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as total amount
@@ -32,11 +35,40 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
 
-the_bill = Bill(amount=120, period=30)
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2),2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1),2))
+
+        pdf = FPDF(orientation="P", unit="pt", format="A4")
+        pdf.add_page()
+
+        # Insert title
+        pdf.set_font(family="Helvetica", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Insert Period label & value
+        pdf.set_font(family="Helvetica", size=16, style="B")
+        pdf.cell(w=0, h=20, txt="Period", border=0, align="C", ln=1)
+        pdf.cell(w=0, h=20, txt=bill.period, border=0, align="C", ln=1)
+
+        # Insert name and the due amount of the first flatmate
+        pdf.set_font(family="Helvetica", size=16, style="B")
+        pdf.cell(w=0, h=20, txt=flatmate1.name, border=0)
+        pdf.cell(w=0, h=20, txt=flatmate1_pay, border=0, align="C",ln=1)
+
+        # Insert name and the due amount of the second flatmate
+        pdf.set_font(family="Helvetica", size=16, style="B")
+        pdf.cell(w=0, h=20, txt=flatmate2.name, border=0)
+        pdf.cell(w=0, h=20, txt=flatmate2_pay, border=0, align="C")
+
+        pdf.output(self.filename)
+
+the_bill = Bill(amount=120, period="April 2021")
 john = Flatmate(name="John", days_in_house=20)
 mary = Flatmate(name="Mary",days_in_house=25)
 
 print("Mary pays:",mary.pays(bill=the_bill, flatmate2=john))
 print("John pays:", john.pays(bill=the_bill,flatmate2=mary))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(john, mary, the_bill)
